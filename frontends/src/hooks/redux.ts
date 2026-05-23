1ce47491176a1f2068a187984455b6f5a237f254
Chukwudi Vincent <chukwudivincent79@gmail.com>
2026-05-23 12:20:16 +0100

    deleted files

--- frontends/src/hooks/redux.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
-import type { AppDispatch, AppRootState } from '../store'
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
